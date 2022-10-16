3103d09ca86831d5ebc3cbc32c8ebd2c11f3efcf
Pranav <pranavkhot77@gmail.com>
2022-10-16 17:12:24 +0530

    UserLogin and Registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index)
+    path('',views.index,name="index"),
+    path('login/',views.Userlogin,name="login"),
+    path('register/',views.UserRegistration,name="register")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from accounts.forms import *
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+
+def Userlogin(request):
+    if request.method=="POST":
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        user=authenticate(request,username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('index')
+
+        else:
+            messages.error(request,"Username or Password is Incorrect")
+
+    return render(request,"index.html")
+
+
+def UserRegistration(request):
+    form=UserCreationForm()
+    if request.method=="POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"User Successfuly created")
+            return redirect("login")
+        else:
+            messages.error(request,"refill")
+    context={
+        "RegistrationForm":form
+    }
+    return render(request,"register.html",context)
