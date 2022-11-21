c5406b1b316313abe9dbb4967ecfd4ee4a99f9a0
Pranav0707 <pranavkhot77@gmail.com>
2022-11-21 12:30:20 +0530

    settings.py changes

--- Disease_Prediction_Platform/settings.py
+++ Disease_Prediction_Platform/settings.py
@@ -126,10 +126,5 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_POST = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='pranavkhot77@gmail.com'
-EMAIL_HOST_PASSWORD ='wfmiqcgungzvmuem'
+
 # EMAIL_USE_SSL=False
