055101e46938ebda9acd35deaceb3674481f7b2f
Pranav0707 <pranavkhot77@gmail.com>
2023-05-04 18:57:10 +0530

    models added in admin panel

--- diseases_app/admin.py
+++ diseases_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from . models import HeartDiseasePrediction
+from . models import HeartDiseasePrediction,KidneyDiseasePrediction,LiverDiseasePrediction,DiabetesDiseasePrediction
 # Register your models here.
 admin.site.register(HeartDiseasePrediction)
+admin.site.register(KidneyDiseasePrediction)
+admin.site.register(LiverDiseasePrediction)
+admin.site.register(DiabetesDiseasePrediction)
